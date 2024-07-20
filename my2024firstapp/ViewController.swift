d2a556c572eb0e443505213f27cd2d3f914a5164
Nouf <Noufmalloboon@gmail.com>
2024-07-20 17:39:39 +0300

    Initial Commit

---
+++ my2024firstapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my2024firstapp
+//
+//  Created by Nouf Alloboon on 14/01/1446 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
